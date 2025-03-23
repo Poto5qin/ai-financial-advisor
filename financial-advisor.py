3156aedbc500791bccdefc527c897258b78098e6
Poto5qin <160687728+Poto5qin@users.noreply.github.com>
2025-03-23 14:08:32 +0800

    Update financial-advisor.py

--- financial-advisor.py
+++ financial-advisor.py
@@ -11,17 +11,20 @@ def fintechbot(question:str):
     db_path = os.path.join(os.getcwd(), "ai-financial-advisor.db")
     db = SQLDatabase.from_uri(f'sqlite:///{db_path}')
 
-   bedrock_config = botocore.config.Config(read_timeout=900, connect_timeout=900, region_name="us-east-1")
+   bedrock_config = botocore.config.Config(
+                    read_timeout=900, 
+                    connect_timeout=900, 
+                    region_name="us-east-1")
    bedrock_client = boto3.client("bedrock-runtime", config=bedrock_config)
 
    llm = ChatBedrock(
-    client=bedrock_client,
-    model_id = "anthropic.claude-3-sonnet-20240229-v1:0",
-    model_kwargs = {
-        "temperature": 0.1,
-        "top_p": 0.7,
-        "top_k": 250,
-    }
+        client=bedrock_client,
+        model_id = "anthropic.claude-3-sonnet-20240229-v1:0",
+        model_kwargs = {
+            "temperature": 0.1,
+            "top_p": 0.7,
+            "top_k": 250,
+        }
     )
    
    agent_executor = create_sql_agent(llm, db=db)
