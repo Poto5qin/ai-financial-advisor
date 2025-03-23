e86101cd47e8ab3175eff361850df21bbfdd4282
Poto5qin <160687728+Poto5qin@users.noreply.github.com>
2025-03-23 14:13:21 +0800

    Update financial-advisor.py

--- financial-advisor.py
+++ financial-advisor.py
@@ -1,3 +1,4 @@
+import os
 import boto3
 import botocore.config
 
@@ -5,38 +6,38 @@ from langchain_aws import ChatBedrock
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.utilities import SQLDatabase
 
-def fintechbot(question:str):
-    
+def fintechbot(question: str):
     # Use the current working directory to reference the database
     db_path = os.path.join(os.getcwd(), "ai-financial-advisor.db")
     db = SQLDatabase.from_uri(f'sqlite:///{db_path}')
+    
+    # Ensure correct indentation here with 4 spaces
+    bedrock_config = botocore.config.Config(read_timeout=900, connect_timeout=900, region_name="us-east-1")
+    bedrock_client = boto3.client("bedrock-runtime", config=bedrock_config)
 
-   bedrock_config = botocore.config.Config(read_timeout=900,connect_timeout=900,region_name="us-east-1")
-   bedrock_client = boto3.client("bedrock-runtime", config=bedrock_config)
-
-   llm = ChatBedrock(
+    llm = ChatBedrock(
         client=bedrock_client,
-        model_id = "anthropic.claude-3-sonnet-20240229-v1:0",
-        model_kwargs = {
+        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        model_kwargs={
             "temperature": 0.1,
             "top_p": 0.7,
             "top_k": 250,
         }
     )
    
-   agent_executor = create_sql_agent(llm, db=db)
-   # agent_executor = create_sql_agent(llm, db=db, verbose=True)
+    agent_executor = create_sql_agent(llm, db=db)
+    # agent_executor = create_sql_agent(llm, db=db, verbose=True)
 
-   response = agent_executor.invoke(
-    {
-        "input": question
-    }
-   )
+    response = agent_executor.invoke(
+        {
+            "input": question
+        }
+    )
 
-   return response['output']
+    return response['output']
 
-if __name__=="__main__":
+if __name__ == "__main__":
     QUESTION = input("Ask a question: ")
     print(fintechbot(QUESTION))
 
-# I have a high-risk tolerance. What investment options would you recommend?
+#
